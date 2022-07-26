b339643304d6f38c91b8fa5fa0c5e5fcc74e7c7e
root <zvichayniy.vick@gmail.com>
2022-07-26 11:38:56 +0300

    Revert "fix css, fix managment"
    This reverts commit 93f93a59ec64d663e6e8a49783344098d2c9c496.

--- static/js/managment.js
+++ static/js/managment.js
@@ -1,15 +1,8 @@
-let showCountQuery = document.querySelector('.managment__show-count input');
-let inputArrows = document.querySelectorAll('.managment__show-count-arrows span');
-let search = document.querySelector('.managment__search input');
+let showCountQuery = document.querySelector('.managment__show-count input')
+let inputArrows = document.querySelectorAll('.managment__show-count-arrows span')
 
-const localValue = localStorage.getItem('searchValue');
-search.value = localValue || '';
-
-search.addEventListener('change', (e) => {
-  localStorage.setItem('searchValue', e.target.value);
-});
-
-inputArrows[0].addEventListener('click', () => (showCountQuery.value = +showCountQuery.value + 1));
+inputArrows[0].addEventListener('click', () => showCountQuery.value = +showCountQuery.value + 1)
 inputArrows[1].addEventListener('click', () => {
-  if (showCountQuery.value > 0) showCountQuery.value = +showCountQuery.value - 1;
-});
+	if (showCountQuery.value > 0) showCountQuery.value = +showCountQuery.value - 1
+})
+
